a0b38d5d0b754378a49834688cce4d630cf06b2b
Emilly <emillyjandirasilvaribreirojandi@gmail.com>
2025-10-30 22:21:11 -0300

    outro

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,15 @@
 console.log("ola ceu")
+//const mediaDeNotas
+//const MediaNotas//
+//const media_notas//Snacke_Case
+let numero = 102 
+// criando variavel 
+// declarando variavel e atribuindo 102
+console.log (numero)
+numero = 99 
+console.log(numero)
 
+const nome= "seu nome"
+console.log(nome)
+
+//nome= "batata"  - nao pode atribuir valor // para constante depois de declarra
